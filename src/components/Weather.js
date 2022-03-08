3e1ec762842994e9a94bb0a2e5e61eb37b3d8652
RobertoFlores132 <crystalgato@yahoo.com>
2022-03-07 18:28:27 -0600

    Se agrega funcion al boton, se cambian estilos

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -16,7 +16,7 @@ const Weather = () => {
         axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=d71d7a28b79954a0883d2794e3501218`)
         .then(res => {
             setResponse(res.data);
-            setTemp(res.data.main.temp);
+            setTemp(res.data.main.temp-273.15);
             setFeels_like(res.data.main.feels_like)
     })}
     
@@ -30,20 +30,14 @@ const Weather = () => {
         navigator.geolocation.getCurrentPosition(success)
     }, [])
 
-    const convertCelsius = () => {
-        const celsius = temp - 273.15
-
-        return celsius.toFixed(2)
-    }
 
     const convertTemp = () => {
-
         if(isCelsius){
-           setTemp(convertCelsius()*1.8+32)
-            setIsCelsius(false)
+           setTemp((temp*1.8)+32);
+            setIsCelsius(false);
         }else{
-            setTemp(convertCelsius()-32*1.8)
-            setIsCelsius(true)
+            setTemp((temp-32)*5/9);
+            setIsCelsius(true);
         }
     }
 
@@ -51,16 +45,16 @@ const Weather = () => {
         <div className='Weather'>
         <div className='Weather-Card'>
             <h1>{response.name}, {response.sys?.country}</h1>
-            <p><i className="fas fa-cloud"></i></p>
+              <h2 className='temp'>{temp.toFixed(2)} {isCelsius ? '°C' : '°F' }</h2>
+              <p><i className="fas fa-cloud"></i></p>
             <div className='div-temp'>
-            <h2>Temperatura Actual: {convertCelsius()} {isCelsius ? '°C' : '°F' }</h2>
-            <h2>Sensacion Térmica: {like()} °C</h2>
-            <h2>Presión Atmosférica: {response.main?.pressure} hPa</h2>
-            <h2>Humedad: {response.main?.humidity}%</h2>
-            <h2>Velocidad del Viento: {response.wind?.speed} m/s</h2>
+                <h2>Sensacion Térmica: {like()} °C</h2>
+                <h2>Presión Atmosférica: {response.main?.pressure} hPa</h2>
+                <h2>Humedad: {response.main?.humidity}%</h2>
+                <h2>Velocidad del Viento: {response.wind?.speed} m/s</h2>
             </div>
-            <div className='Button'>
-            <button onClick={convertTemp}>Convert to {isCelsius ? '°F' : '°C'}</button>
+            <div className='button'>
+                <button onClick={convertTemp}><i class="fas fa-exchange"></i>{isCelsius ? '°F' : '°C'}</button>
             </div>
         </div>
         </div>
